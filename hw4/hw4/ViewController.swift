163e441781730766347b67a7ac16f432654368fb
robertmukhtarov-kfu <rrmukhtarov@stud.kpfu.ru>
2021-11-08 16:18:20 +0300

    initial commit

---
+++ hw4/hw4/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  hw4
+//
+//  Created by Robert Mukhtarov on 08.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
